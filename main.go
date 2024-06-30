9438c95e892067872e37cefc661ad06f39a6b63b
Wendell Souza <wendell.souza055283@gmail.com>
2024-06-30 17:46:50 -0300

    novo commit

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello Full Cycle</h1>")
+	fmt.Fprintf(w, "<h1>Hello Argo e Full Cycle</h1>")
 }
 
 func main() {
